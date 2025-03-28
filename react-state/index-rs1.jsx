60f29da55ad38d95938b032f4889fbf731640feb
Sara Zmak <zmaksara@gmail.com>
2025-03-28 14:51:45 +0100

    add code from class

--- react-state/index-rs1.jsx
+++ react-state/index-rs1.jsx
@@ -9,3 +9,34 @@ arrow functions:
 if we have a return then we need curly:
 () => { return }
 */
+
+import React from 'react';
+import PokemonCard from './components/PokemonCard';
+import './App.css';
+import './styles.css';
+
+import { pokemons } from './utils/data';
+
+export default function App() {
+	// const pokemons = undefined;
+	console.log('pokemons', pokemons);
+
+	return (
+		<main className="app">
+			<h1 className="app__title">Pokédex</h1>
+			<ul className="app__pokemons">
+				{pokemons?.map((pokemon, index) => (
+					// <li key={pokemon.id}>
+					<li key={index}>
+						<PokemonCard
+							id={pokemon.id}
+							name={pokemon.name}
+							types={pokemon.types}
+							image={pokemon.image}
+						/>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+}
