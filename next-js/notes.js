3df98025dfd22f8e21573f240ffa9428ac55b4d4
Sara Zmak <zmaksara@gmail.com>
2025-04-08 11:06:52 +0200

    notes files

---
+++ next-js/notes.js
@@ -0,0 +1,10 @@
+/* 
+Next JS - a framework from React
+
+if you have a small project, next js could be an overkill
+if there is a lot of routing than it would be a good fit 
+
+Vite - also a framework but cant be used with next js
+
+no need to modify the styles folders and document.js
+*/
